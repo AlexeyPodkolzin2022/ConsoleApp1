a533aeb715c65244dba665f9072167d66e4854bc
AlexeyPodkolzin2022 <Alice@HOME-PC>
2022-07-25 23:05:16 +0300

    Добавлен код

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
+            Console.WriteLine("Hello2");//сделали какое-то изменение
         }
     }
 }
